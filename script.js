1ba0d851fa2127767f0046c45c7af18ff68588cb
MGutix <140160200+MGutix@users.noreply.github.com>
2023-10-12 10:49:55 +0900

    Simple visuals

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function GameBoard() {
+function Gameboard() {
     const rows = 3;
     const columns = 3;
     const board = [];
+    const canvas = document.getElementById('canvas')
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(/*celda */)
+            board[i].push('')
             
         }
         
@@ -17,7 +18,18 @@ function GameBoard() {
     // playerChoice  { if (occupied === invalid || empty => change to player symbol)}
 
     const renderBoard = () => {
-        // 
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                const cell = document.createElement("button");
+                cell.classList.add("cell");
+                cell.textContent = board[i][j];
+
+                canvas.appendChild(cell);
+                
+            }
+            
+        }
+        
     }
 
     return {getBoard, renderBoard, /*playerChoice*/}
@@ -32,6 +44,10 @@ function GameFlow() {
     // create players
 
     // render board
+    const board = Gameboard();
+    const printNewRound = () => {
+        board.printBoard();
+      };
 
     // recieve playerInput => make changes to board (re render) + change turn
 
